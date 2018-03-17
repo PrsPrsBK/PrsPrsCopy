9f36472b6de3254f5a9c778a418efb3659331115
PrsPrsBK <prsprsbk@gmail.com>
2018-03-17 20:18:02 +0900

    feat: options_ui

--- options_ui/options.js
+++ options_ui/options.js
@@ -3,11 +3,22 @@ const onError = (error) => {
 };
 
 const registAdd = (e) => {
+};
+
+const addEntry = (e) => {
   e.preventDefault();
   const date = new Date();
-  const set_obj = {};
-  set_obj['seconds'] = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-  browser.storage.local.set(set_obj);
+  const aElm = document.createElement('textarea');
+  const wkVal = date.getMinutes() + ':' + date.getSeconds();
+  console.log(wkVal);
+  aElm.id = wkVal;
+  aElm.value = wkVal;
+  const targetElm = document.getElementById('under_edit');
+  targetElm.appendChild(aElm);
+
+  //const set_obj = {};
+  //set_obj['seconds'] = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+  //browser.storage.local.set(set_obj);
 };
 
 const registModify = (e) => {
@@ -18,13 +29,14 @@ const restoreEntries = (e) => {
   const showContents = (result) => {
   };
 
-  const getting = browser.storage.local.get(some_key);
-  getting.then(showContents, onError);
+  //const getting = browser.storage.local.get(some_key);
+  //getting.then(showContents, onError);
 };
 
 document.addEventListener('DOMContentLoaded', restoreEntries);
-document.querySelector('form').addEventListener('submit', someFunc);
-document.querySelector('#refist_add').addEventListener('click', registAdd);
-document.querySelector('#refist_modify').addEventListener('click', registModify);
+//document.querySelector('form').addEventListener('submit', someFunc);
+document.querySelector('#add_template').addEventListener('click', addEntry);
+document.querySelector('#regist_add').addEventListener('click', registAdd);
+//document.querySelector('#regist_modify').addEventListener('click', registModify);
 // vim:expandtab ff=dos fenc=utf-8 sw=2
 
