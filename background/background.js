83bb0f475e321280a007af261c832d86c9cac14e
PrsPrsBK <prsprsbk@gmail.com>
2018-04-20 19:41:42 +0900

    feat: copy facility for twitter

--- background/background.js
+++ background/background.js
@@ -1,6 +1,17 @@
-/*global browser:true*/
-
+console.log('ppcopy');
 if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
+
+browser.tabs.onUpdated.addListener((tabId, chgInfo, tab) => {
+  console.log(`ppcopy ${tab.url}`);
+  if(tab.url.startsWith('https://twitter.com')) {
+    browser.tabs.executeScript(tabId, {
+      file: '/content_scripts/TextPicker.js',
+    });
+  }
+});
+
+console.log('ppcopy');
+// vim:expandtab ff=dos fenc=utf-8 sw=2
 
