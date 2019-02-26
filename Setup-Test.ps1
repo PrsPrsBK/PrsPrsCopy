5e6663d1bcb2a53a5a6a0bea44eda5b02b29ad54
PrsPrsBK <prsprsbk@gmail.com>
2019-02-26 18:46:53 +0900

    test: give up auto-generated test-targets

--- Setup-Test.ps1
+++
@@ -1,3 +0,0 @@
-New-Item ./testTgt -ItemType "directory" -Force | Out-Null
-Copy-Item ./content_scripts/textPicker.js ./testTgt/textPicker.js
-Get-Content ./resources/textPicker.suffix.js | Add-Content -Path ./testTgt/textPicker.js
