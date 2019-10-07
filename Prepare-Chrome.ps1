d7024ac7c5403b181eae046a6b7e4eb44584f51e
PrsPrsBK <prsprsbk@gmail.com>
2019-10-07 16:21:43 +0900

    ソースコードをChromeとFirefoxで共有

--- Prepare-Chrome.ps1
+++ Prepare-Chrome.ps1
@@ -1,6 +1,35 @@
 $script_path =  (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
 New-Item ./lib -ItemType "directory" -Force | Out-Null
-
 Copy-Item ./node_modules/webextension-polyfill/dist/browser-polyfill.min.js ./chrome/lib/
+
+New-Item chrome -ItemType "directory" -Force | Out-Null
+New-Item chrome/background -ItemType "directory" -Force | Out-Null
+New-Item chrome/content_scripts -ItemType "directory" -Force | Out-Null
+New-Item chrome/icons -ItemType "directory" -Force | Out-Null
+New-Item chrome/options_ui -ItemType "directory" -Force | Out-Null
+New-Item chrome/popup -ItemType "directory" -Force | Out-Null
+
+Copy-Item src/background/background.js chrome/background/background.js
+Copy-Item src/content_scripts/textPicker.js chrome/content_scripts/textPicker.js
+Copy-Item src/icons/icon-48_0.png chrome/icons/icon-48_0.png
+Copy-Item src/icons/icon-48_1.png chrome/icons/icon-48_1.png
+Copy-Item src/icons/icon-48_app.png chrome/icons/icon-48_app.png
+Copy-Item src/options_ui/options.css chrome/options_ui/options.css
+Copy-Item src/options_ui/options.html chrome/options_ui/options.html
+Copy-Item src/options_ui/options.js chrome/options_ui/options.js
+Copy-Item src/popup/menu.css chrome/popup/menu.css
+Copy-Item src/popup/menu.html chrome/popup/menu.html
+Copy-Item src/popup/menu.js chrome/popup/menu.js
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Prepare-Firefox.ps1
@@ -0,0 +1,35 @@
+$script_path =  (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+#New-Item ./lib -ItemType "directory" -Force | Out-Null
+#Copy-Item ./node_modules/webextension-polyfill/dist/browser-polyfill.min.js ./firefox/lib/
+
+New-Item firefox -ItemType "directory" -Force | Out-Null
+New-Item firefox/background -ItemType "directory" -Force | Out-Null
+New-Item firefox/content_scripts -ItemType "directory" -Force | Out-Null
+New-Item firefox/icons -ItemType "directory" -Force | Out-Null
+New-Item firefox/options_ui -ItemType "directory" -Force | Out-Null
+New-Item firefox/popup -ItemType "directory" -Force | Out-Null
+
+Copy-Item src/background/background.js firefox/background/background.js
+Copy-Item src/content_scripts/textPicker.js firefox/content_scripts/textPicker.js
+Copy-Item src/icons/icon-48_0.png firefox/icons/icon-48_0.png
+Copy-Item src/icons/icon-48_1.png firefox/icons/icon-48_1.png
+Copy-Item src/icons/icon-48_app.png firefox/icons/icon-48_app.png
+Copy-Item src/options_ui/options.css firefox/options_ui/options.css
+Copy-Item src/options_ui/options.html firefox/options_ui/options.html
+Copy-Item src/options_ui/options.js firefox/options_ui/options.js
+Copy-Item src/popup/menu.css firefox/popup/menu.css
+Copy-Item src/popup/menu.html firefox/popup/menu.html
+Copy-Item src/popup/menu.js firefox/popup/menu.js
+
+
+
+
+
+
+
+
+
+
+
+
