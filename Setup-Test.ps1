4c142c9bd7f641783338eceabb82bbb42f3912e2
PrsPrsBK <prsprsbk@gmail.com>
2021-01-27 20:24:46 +0900

    テストセットアップスクリプト修正

--- Setup-Test.ps1
+++ Setup-Test.ps1
@@ -1,3 +1,3 @@
 New-Item ./testTgt -ItemType "directory" -Force | Out-Null
-Copy-Item ./content_scripts/textPicker.js ./testTgt/textPicker.js
+Copy-Item ./src/content_scripts/textPicker.js ./testTgt/textPicker.js
 Get-Content ./resources/textPicker.suffix.js | Add-Content -Path ./testTgt/textPicker.js
