2811bf1575ced0ddb038579e861a411a1081dc7b
PrsPrsBK <prsprsbk@gmail.com>
2021-09-18 23:37:08 +0900

    2021-09-10 HTML変更 リプライ

--- src/content_scripts/textPicker.js
+++ src/content_scripts/textPicker.js
@@ -255,14 +255,14 @@ const tweetPicker = {
         }
         const unlessReplySelector = tweetPicker.CUR_IS_PICKUP
           ? ':scope div[data-testid="tweet"] + div > div:nth-child(1) > div > div'
-          : ':scope div[data-testid="tweet"] > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div';
+          : ':scope > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div';
         const textUnlessReply = tweetPicker.CUR_ARTICLE.querySelector(unlessReplySelector);
         if(textUnlessReply?.hasAttribute('lang') === false) {
           tweetPicker.CUR_IS_REPLY = true;
         }
         const qtTextSelector = tweetPicker.CUR_IS_PICKUP
           ? `:scope > div > div:nth-child(${tweetPicker.CUR_IS_REPLY ? 5 : 4}) div[role="blockquote"]`
-          : `:scope div[data-testid="tweet"] > div:nth-child(2) > div:nth-child(2) > div:nth-child(${tweetPicker.CUR_IS_REPLY ? 3 : 2}) > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div`;
+          : `:scope > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(${tweetPicker.CUR_IS_REPLY ? 3 : 2}) > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div`;
         const qtTextElm = tweetPicker.CUR_ARTICLE.querySelector(qtTextSelector);
         tweetPicker.CUR_HAS_QT = qtTextElm?.hasAttribute('lang');
         tweetPicker.prepareCurText(tweetPicker.CUR_ARTICLE);
